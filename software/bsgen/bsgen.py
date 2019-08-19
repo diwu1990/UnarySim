26e2b1a110d6d9f8d1021e8a312458b8453b6d37
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2019-08-18 23:55:39 -0700

    bsgen

---
+++ software/bsgen/bsgen.py
@@ -0,0 +1,28 @@
+import torch
+
+class BSGen(object):
+    """
+    compare source data with rng_seq[rng_idx] to generate bit stream from source
+    """
+    def __init__(self, source, rng_seq):
+        super(BSGen, self).__init__()
+        self.source = source
+        self.rng_seq = rng_seq
+    
+    def Gen(self, rng_idx):
+        return torch.gt(self.source, self.rng_seq[rng_idx]).type(torch.int8)
+
+class BSRegen(object):
+    """
+    collect input bit, compare buffered binary with rng_seq[rng_idx] to regenerate bit stream
+    """
+    def __init__(self, input_shape, depth, rng_seq):
+        super(BSGen, self).__init__()
+        self.input_shape = input_shape
+        self.rng_seq = rng_seq
+        self.half = pow(2,depth-1)
+        self.cnt = torch.ones(input_shape).mul_(self.half).type(type(torch.long))
+    
+    def Gen(self, in_bit):
+        self.cnt.add_(in_bit.mul_(2).sub_(1))
+        return torch.gt(self.cnt, self.rng_seq[rng_idx]).type(torch.int8)
