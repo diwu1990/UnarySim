87c75ab7e11d4e78ebeccc3f2513857c2c5ffecc
diwu1990 <wudidaizi@hotmail.com>
2019-12-06 23:36:33 -0600

    fix bugs in shiftregister

--- sw/kernel/shiftreg.py
+++ sw/kernel/shiftreg.py
@@ -3,20 +3,33 @@ import torch
 class ShiftReg(torch.nn.Module):
     """
     this module is a shift register for int8 tensor.
+    it takes in unary bits and output shifter register value, as well as the total number of 1s in current shift register.
     """
     def __init__(self,
                  depth=8):
         super(ShiftReg, self).__init__()
         self.depth = 8
         self.sr = torch.nn.Parameter(torch.zeros(1).type(torch.int8), requires_grad=False)
+        self.init = True
 
     def ShiftReg_forward(self, input):
-        if self.sr.shape == [self.depth, input.shape]:
-            # shape does not match, do broadcasting and initialization
-            pass
+        if self.init is True:
+            new_shape = [1 for _ in range(len(input.shape))]
+            new_shape.insert(0, self.depth)
+            self.sr.data = input.repeat(new_shape)
+            for i in range(self.depth):
+                self.sr[i].fill_(i%2)
+            self.init = False
         
         # do shifting
-                 
+        # output
+        out = self.sr[0]
+        # sum in current shift register
+        cnt = torch.sum(self.sr, 0)
+        self.sr.data = torch.roll(self.sr, -1, 0)
+        self.sr.data[self.depth-1] = input.clone().detach()
+        return out, cnt
+
     def forward(self, input):
         return self.ShiftReg_forward(input)
 
