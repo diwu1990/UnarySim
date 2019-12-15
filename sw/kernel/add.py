6b29a7cd00dccad7574b1c6c0e66cfa8f0fb5b54
diwu1990 <wudidaizi@hotmail.com>
2019-12-14 19:08:14 -0600

    add Gaines add, not test yet

--- sw/kernel/add.py
+++ sw/kernel/add.py
@@ -48,3 +48,37 @@ class UnaryAdd(torch.nn.Module):
 
         return self.output.type(torch.int8)
         
+
+class GainesAdd(torch.nn.Module):
+    """
+    this module is for Gaines addition, just MUX-based adder.
+    """
+    def __init__(self, 
+                 mode="bipolar", 
+                 scaled=True, 
+                 acc_dim=0):
+        super(UnaryAdd, self).__init__()
+        
+        # data representation
+        self.mode = mode
+        # whether it is scaled addition
+        self.scaled = scaled
+        # dimension to do reduce sum
+        self.acc_dim = torch.nn.Parameter(torch.zeros(1).type(torch.int8), requires_grad=False)
+        self.acc_dim.fill_(acc_dim)
+        
+    def forward(self, input):
+        self.acc_bound.fill_(input.size()[self.acc_dim.item()])
+        if self.mode is "bipolar":
+            self.offset.fill_((self.acc_bound.item()-1)/2)
+        self.accumulator.data = self.accumulator.add(torch.sum(input.type(torch.float), self.acc_dim.item()))
+        
+        if self.scaled is True:
+            self.output = torch.ge(self.accumulator, self.acc_bound.item()).type(torch.float)
+            self.accumulator.sub_(self.output * self.acc_bound)
+        else:
+            self.accumulator.sub_(self.offset)
+            self.output = torch.gt(self.accumulator, self.out_accumulator).type(torch.float)
+            self.out_accumulator.data = self.out_accumulator.add(self.output)
+
+        return self.output.type(torch.int8)
