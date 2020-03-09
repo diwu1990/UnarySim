b72e3263f8d3d6d764c25260bcc572da1f54f125
diwu1990 <diwu1990@github.com>
2020-03-09 11:35:47 -0500

    update shiftreg to support masked input

--- sw/kernel/shiftreg.py
+++ sw/kernel/shiftreg.py
@@ -10,11 +10,11 @@ class ShiftReg(torch.nn.Module):
                  bstype=torch.float):
         super(ShiftReg, self).__init__()
         self.depth = 8
-        self.sr = torch.nn.Parameter(torch.zeros(1).type(bstype), requires_grad=False)
+        self.sr = torch.nn.Parameter(torch.tensor([x%2 for x in range(0, depth)]).type(bstype), requires_grad=False)
         self.init = True
         self.bstype = bstype
 
-    def ShiftReg_forward(self, input, index=0):
+    def ShiftReg_forward(self, input, mask=None, index=0):
         if self.init is True:
             new_shape = [1 for _ in range(len(input.shape))]
             new_shape.insert(0, self.depth)
@@ -28,11 +28,19 @@ class ShiftReg(torch.nn.Module):
         out = self.sr[index]
         # sum in current shift register
         cnt = torch.sum(self.sr, 0)
-        self.sr.data = torch.roll(self.sr, -1, 0)
-        self.sr.data[self.depth-1] = input.clone().detach()
+        if mask is None:
+            self.sr.data = torch.roll(self.sr, -1, 0)
+            self.sr.data[self.depth-1] = input.clone().detach()
+        else:
+            assert mask.size() == input.size(), "size of the enable mask unmatches that of input"
+            mask_val = mask.type(self.bstype)
+            sr_shift = torch.roll(self.sr, -1, 0)
+            sr_shift[self.depth-1] = input.clone().detach()
+            sr_no_shift = self.sr.clone().detach()
+            self.sr.data = mask_val * sr_shift + (1 - mask_val) * sr_no_shift
         return out, cnt
 
-    def forward(self, input, index=0):
-        return self.ShiftReg_forward(input, index)
+    def forward(self, input, mask=None, index=0):
+        return self.ShiftReg_forward(input, mask=mask, index=index)
 
     
