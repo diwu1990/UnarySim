d6158905fa9306de32d0132191405de587d3b916
diwu1990 <diwu1990@github.com>
2020-03-31 11:24:33 -0500

    add sign and test

---
+++ sw/kernel/sign.py
@@ -0,0 +1,48 @@
+import torch
+from UnarySim.sw.kernel.shiftreg import ShiftReg
+
+class UnarySign(torch.nn.Module):
+    """
+    return the sign of unary bipolar rate-coded data
+    0 for non-negative, and 1 for negative
+    """
+    def __init__(self, depth=4, shiftreg=False, bstype=torch.float, buftype=torch.float):
+        super(UnarySign, self).__init__()
+        self.depth = depth
+        self.depth_half = torch.nn.Parameter(torch.zeros(1).fill_(depth/2).type(buftype), requires_grad=False)
+        self.sr = shiftreg
+        self.bstype = bstype
+        self.buftype = buftype
+        if shiftreg is True:
+            assert depth <= 127, "When using shift register implementation, buffer depth should be less than 127."
+            self.shiftreg = ShiftReg(depth, self.bstype)
+            self.sr_cnt = torch.nn.Parameter(torch.zeros(1).type(self.bstype), requires_grad=False)
+            self.init = True
+        else:
+            self.buf_max = torch.nn.Parameter(torch.zeros(1).fill_(2**depth - 1).type(buftype), requires_grad=False)
+            self.buf_half = torch.nn.Parameter(torch.zeros(1).fill_(2**(depth - 1)).type(buftype), requires_grad=False)
+            self.acc = torch.nn.Parameter(torch.zeros(1).fill_(2**(depth - 1)).type(buftype), requires_grad=False)
+
+    def UnarySign_forward_rc(self, input):
+        # check whether acc is larger than or equal to half.
+        output = torch.lt(self.acc, self.buf_half).type(self.bstype)
+        # update the accumulator
+        self.acc.data = self.acc.add(input.mul(2).sub(1).type(self.buftype)).clamp(0, self.buf_max.item())
+        return output
+    
+    def UnarySign_forward_rc_sr(self, input):
+        # check whether sr sum is larger than or equal to half.
+        if self.init is True:
+            output = torch.ones_like(input).type(self.bstype)
+            self.init = False
+        else:
+            output = torch.lt(self.sr_cnt, self.depth_half).type(self.bstype)
+        # update shiftreg
+        _, self.sr_cnt.data = self.shiftreg(input)
+        return output
+
+    def forward(self, input):
+        if self.sr is False:
+            return self.UnarySign_forward_rc(input)
+        else:
+            return self.UnarySign_forward_rc_sr(input)
