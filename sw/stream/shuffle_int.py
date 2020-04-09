2d8e11fbcef22ab71347d931593fe8a842034854
diwu1990 <diwu1990@github.com>
2020-04-08 21:02:31 -0500

    update skewedsyncint and  test

--- sw/stream/shuffle_int.py
+++ sw/stream/shuffle_int.py
@@ -10,24 +10,20 @@ class SkewedSyncInt(torch.nn.Module):
         self.buftype=buftype
         self.stype=stype
         self.upper = torch.nn.Parameter(torch.Tensor([pow(2, depth) - 1]).type(self.buftype), requires_grad=False)
-        self.cnt = torch.nn.Parameter(torch.Tensor(1).type(self.buftype), requires_grad=False)
-        self.out_1 = torch.nn.Parameter(torch.Tensor(1).type(self.stype), requires_grad=False)
-        self.cnt_not_min = torch.nn.Parameter(torch.Tensor(1).type(self.stype), requires_grad=False)
-        self.cnt_not_max = torch.nn.Parameter(torch.Tensor(1).type(self.stype), requires_grad=False)
+        self.cnt = torch.nn.Parameter(torch.zeros(1).type(self.buftype), requires_grad=False)
         
     def forward(self, in_1, in_2):
         # input 2 is kept unchanged at output
         # if input 1 is smaller than input 2, this module works the same as SkewedSync
         # if input 1 is larger than input 2, this module aggregates 1s in input 1 to integers larger than 1, and sync them with input 2
         # output 1 is a binary digit stream.
-        in_2_eq_1 = torch.eq(in_2, 1).type(torch.int8)
-        sum_in = in_1 + in_2
-        if list(self.cnt.size()) != list(sum_in.size()):
-            self.cnt.data = torch.zeros_like(sum_in).type(self.buftype)
-        self.cnt_not_min.data = torch.ne(self.cnt, 0).type(self.stype)
-        self.cnt_not_max.data = torch.ne(self.cnt, self.upper.item()).type(self.stype)
+        
+        in_2_eq_1 = torch.eq(in_2, 1).type(self.buftype)
+        # when input 2 is 1, output 1 depends on the sum of the current cnt and the current input 1, otherwise output 1 is 0
+        temp_sum = self.cnt + in_1.type(self.buftype)
+        temp_sum_gt_max = torch.gt(temp_sum, self.upper.item()).type(self.buftype)
+        out_1 = in_2_eq_1 * (temp_sum_gt_max * self.upper + (1 - temp_sum_gt_max) * temp_sum)
+        self.cnt.data = (temp_sum - out_1).clamp(0, self.upper.item())
 
-        self.out_1.data = in_1.add(torch.eq(sum_in, 1).type(self.stype).mul_(self.cnt_not_min * (1 - in_1) + (0 - self.cnt_not_max) * in_1))
-        self.cnt.data.add_(torch.eq(sum_in, 1).type(self.buftype).mul_(in_1.mul(2).sub(1).type(self.buftype))).clamp_(0, self.upper.item())
-        return self.out_1, in_2
+        return out_1.type(self.stype), in_2
     
