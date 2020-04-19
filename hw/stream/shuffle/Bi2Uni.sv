72bd28648a3d8eacfb2f554da7bfbdfa4add8efb
diwu1990 <diwu1990@github.com>
2020-04-19 18:46:47 -0500

    add u2b b2u rtl

---
+++ hw/stream/shuffle/Bi2Uni.sv
@@ -0,0 +1,26 @@
+module Bi2Uni # (
+    parameter DEP=4
+) (
+    input clk,    // Clock
+    input rst_n,  // Asynchronous reset active low
+    input in,
+    output out
+);
+    // bipolar: output = 2 * input - 1
+    logic [DEP-1:0] diff_acc;
+    logic [1:0] pc;
+
+    assign pc = {in, 1'b0};
+
+    always_ff @(posedge clk or negedge rst_n) begin : proc_diff_acc
+        if(~rst_n) begin
+            diff_acc <= {1'b1, {{DEP-1}{1'b0}}};
+        end else begin
+            diff_acc <= diff_acc + pc - out;
+        end
+    end
+
+    // as long as diff_acc is more than reset value, output a zero.
+    assign out = diff_acc[DEP-1] & |diff_acc[DEP-2:0];
+
+endmodule
---
+++ hw/stream/shuffle/Uni2Bi.sv
@@ -0,0 +1,24 @@
+module Uni2Bi # (
+    input clk,    // Clock
+    input rst_n,  // Asynchronous reset active low
+    input in,
+    output out
+);
+    // bipolar: output = (input + 1)/2
+    logic [1:0] in_acc;
+    logic [1:0] pc;
+
+    assign pc = in + 1'b1;
+
+    always_ff @(posedge clk or negedge rst_n) begin : proc_in_acc
+        if(~rst_n) begin
+            in_acc <= 2'b0;
+        end else begin
+            in_acc <= in_acc[0] + pc;
+        end
+    end
+
+    // output a zero when in_acc overflows.
+    assign out = in_acc[1];
+
+endmodule
