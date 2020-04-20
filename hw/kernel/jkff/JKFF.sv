8d5ae18e55aa90233be9cec6964ba326b11f065c
diwu1990 <diwu1990@github.com>
2020-04-19 22:08:33 -0500

    add jkff rtl

---
+++ hw/kernel/jkff/JKFF.sv
@@ -0,0 +1,25 @@
+module JKFF (
+    input clk,    // Clock
+    input rst_n,  // Asynchronous reset active low
+    input J,
+    input K,
+    output out
+);
+    
+    logic out;
+
+    always_ff @(posedge clk or negedge rst_n) begin : proc_out
+        if(~rst_n) begin
+            out <= 0;
+        end else begin
+            case ({J,K})
+                2'b00: out <= out;
+                2'b10: out <= 1'b1;
+                2'b01: out <= 1'b0;
+                2'b11: out <= ~out;
+                default : out <= 0;
+            endcase
+        end
+    end
+
+endmodule
