d555545a485251f9d5bb3ab5900970cbf3b1f4ae
diwu1990 <diwu1990@github.com>
2020-04-22 15:27:36 -0500

    add cordiv rtl

--- hw/kernel/div/CORDIV_kernel.sv
+++ hw/kernel/div/CORDIV_kernel.sv
@@ -1,17 +1,54 @@
+// module CORDIV_kernel #(
+//     parameter BW=1,
+//     parameter DEP=2,
+//     parameter DEPLOG=1
+// ) (
+//     input logic clk,    // Clock
+//     input logic rst_n,  // Asynchronous reset active low
+//     input logic [DEPLOG-1:0] randNum,
+//     input logic [BW-1:0] dividend,
+//     input logic divisor,
+//     output logic [BW-1:0] quotient
+// );
+    
+//     logic [DEP-1:0] sr [BW-1:0];
+//     logic [1:0] mux [BW-1:0];
+//     logic sel;
+
+//     // when DEP=2 or DEP=1, both DEPLOG sbould be 1.
+//     assign mux[0] = sr[randNum];
+//     assign mux[1] = dividend;
+//     assign sel = divisor;
+
+//     assign quotient = sel ? mux[1] : mux[0];
+
+//     always_ff @(posedge clk or negedge rst_n) begin : proc_sr
+//         if(~rst_n) begin
+//             for (int i = 0; i < DEP; i++) begin
+//                 sr[i] <= i%2*(2^BW-1);
+//             end
+//         end else begin
+//             sr[DEP-1] <= quotient;
+//             for (int i = 0; i < DEP-1; i++) begin
+//                 sr[i] <= sr[i+1];
+//             end
+//         end
+//     end
+
+// endmodule
+
 module CORDIV_kernel #(
-    parameter BW=1,
-    parameter DEP=2,
-    parameter DEPLOG=1
+    parameter BW=1
 ) (
     input logic clk,    // Clock
     input logic rst_n,  // Asynchronous reset active low
-    input logic [DEPLOG-1:0] randNum,
+    input logic randNum,
     input logic [BW-1:0] dividend,
     input logic divisor,
     output logic [BW-1:0] quotient
 );
     
-    logic [DEP-1:0] sr [BW-1:0];
+    logic [1:0] sr [BW-1:0];
     logic [1:0] mux [BW-1:0];
     logic sel;
 
@@ -24,14 +61,10 @@ module CORDIV_kernel #(
 
     always_ff @(posedge clk or negedge rst_n) begin : proc_sr
         if(~rst_n) begin
-            for (int i = 0; i < DEP; i++) begin
-                sr[i] <= i%2*(2^BW-1);
-            end
+            sr[0] <= 0;
+            sr[1] <= 1;
         end else begin
-            sr[DEP-1] <= quotient;
-            for (int i = 0; i < DEP-1; i++) begin
-                sr[i] <= sr[i+1];
-            end
+            sr <= {quotient, sr[1]};
         end
     end
 
