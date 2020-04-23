d7c90938542f16c574b569989d3e416030aa745c
diwu1990 <diwu1990@github.com>
2020-04-22 22:04:17 -0500

    update sqrt rtl

--- hw/kernel/sign/Sign.sv
+++ hw/kernel/sign/Sign.sv
@@ -8,14 +8,19 @@ module Sign # (
 );
     
     logic [DEP-1:0] cnt;
+    logic cntFull;
+    logic cntEmpty;
+
+    assign cntFull = &cnt;
+    assign cntEmpty = ~(|cnt);
 
     always_ff @(posedge clk or negedge rst_n) begin : proc_cnt
         if(~rst_n) begin
             cnt <= {1'b1, {{DEP-1}{1'b0}}};
         end else begin
-            if(value & ~&cnt) begin
+            if(value & ~cntFull) begin
                 cnt <= cnt + 1;
-            end else if(~value & ~|cnt) begin
+            end else if(~value & ~cntEmpty) begin
                 cnt <= cnt - 1;
             end else begin
                 cnt <= cnt;
