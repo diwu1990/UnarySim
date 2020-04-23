26ea5a697764ed18b11aff1fc5315f33b3056acf
diwu1990 <diwu1990@github.com>
2020-04-22 21:50:18 -0500

    update abs rtl

--- hw/kernel/abs/Abs.sv
+++ hw/kernel/abs/Abs.sv
@@ -12,7 +12,7 @@ module Abs # (
 
     always_ff @(posedge clk or negedge rst_n) begin : proc_cnt
         if(~rst_n) begin
-            cnt <= 1 << (DEP-1);
+            cnt <= 1'b1 << (DEP-1);
         end else begin
             if(value & ~&cnt) begin
                 cnt <= cnt + 1;
