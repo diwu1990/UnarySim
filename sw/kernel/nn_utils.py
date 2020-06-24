345d49e36bdd4203e519c4b8a967931a7586a550
diwu1990 <diwu1990@github.com>
2020-06-23 21:13:01 -0500

    update MLP model and train file

--- sw/kernel/nn_utils.py
+++ sw/kernel/nn_utils.py
@@ -140,26 +140,28 @@ class LeNet_clamp(nn.Module):
         x = self.fc3(x)
         return x
     
-
+    
 class MLP3(nn.Module):
-    def __init__(self):
+    def __init__(self, width=512, p=0.5):
         super(MLP3, self).__init__()
-        self.fc1 = nn.Linear(32*32, 512)
-        self.fc2 = nn.Linear(512, 512)
-        self.fc3 = nn.Linear(512, 10)
+        self.fc1 = nn.Linear(32*32, width)
+        self.fc2 = nn.Linear(width, width)
+        self.fc3 = nn.Linear(width, 10)
         
         self.fc1_out = torch.zeros(1)
+        self.do1 = nn.Dropout(p=p)
         self.relu1_out = torch.zeros(1)
         self.fc2_out = torch.zeros(1)
+        self.do2 = nn.Dropout(p=p)
         self.relu2_out = torch.zeros(1)
         self.fc3_out = torch.zeros(1)
 
     def forward(self, x):
         x = x.view(-1, 32*32)
         self.fc1_out = self.fc1(x)
-        self.relu1_out = F.relu(self.fc1_out)
+        self.relu1_out = F.relu(self.do1(self.fc1_out))
         self.fc2_out = self.fc2(self.relu1_out)
-        self.relu2_out = F.relu(self.fc2_out)
+        self.relu2_out = F.relu(self.do2(self.fc2_out))
         self.fc3_out = self.fc3(self.relu2_out)
         return F.softmax(self.fc3_out, dim=1)
     
