68fc5c37adf5d53548c287e11f3f8bf4d2cd602f
diwu1990 <diwu0111@hotmail.com>
2021-11-02 13:16:35 -0500

    update model acc

--- app/uBrain/model_eval/model_eval_script_8_12.sh
+++ app/uBrain/model_eval/model_eval_script_8_12.sh
@@ -21,8 +21,13 @@ done
 for i in $bandwidth_list; do
     let bitwidth_tc=$i
     let bitwidth_rc=$i
-    let depa=$i+4
-    let depm=$i-2
+    let depa=$bitwidth_rc+4
+    if (($bitwidth_rc < 12));
+    then
+        let depm=6
+    else
+        let depm=7
+    fi
     echo "python /home/diwu/Project/UnarySim/app/uBrain/model_eval/model_eval_hub.py --task_mi -i=$system --rnn_hard -bwtc=$bitwidth_tc -bwrc=$bitwidth_rc -bstest=64 -depa=$depa -depm=$depm > /home/diwu/Project/UnarySim/app/uBrain/model_eval/log_hub_$i.log"
     python /home/diwu/Project/UnarySim/app/uBrain/model_eval/model_eval_hub.py --task_mi -i=$system --rnn_hard -bwtc=$bitwidth_tc -bwrc=$bitwidth_rc -bstest=64 -depa=$depa -depm=$depm > /home/diwu/Project/UnarySim/app/uBrain/model_eval/log_hub_$i.log
 done
