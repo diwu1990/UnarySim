2b2b4f8cb8704616d8b1d05a926bd10bab4d031b
diwu1990 <diwu0111@hotmail.com>
2021-11-07 16:36:48 -0600

    update systolic array data extraction

--- app/uBrain/hw/systolic_array/data_extract.py
+++ app/uBrain/hw/systolic_array/data_extract.py
@@ -5,30 +5,58 @@ import math
 import matplotlib
 from numpy import mean, median
 
-def data_extract(technode=""):
-    font = {'family':'Times New Roman', 'size': 6}
-    matplotlib.rc('font', **font)
+def data_extract():
 
-    arch_list = ["eyeriss"]
-    network_list = ["uBrain"]
-    bit_list = ["8"]
-    ram_list = ["ddr3_w__spm"]
+    dir_root = "/home/diwu/Project/UnarySim/app/uBrain/hw/systolic_array/result/"
+    run_prefix = "eyeriss_08b_bp_001c_uBrain_t"
+    run_postfix = "_ddr3_w__spm"
+    total_timestamp = 10
 
-    cycle_list = []
+    area_list = []
+    runtime_list = []
+    energy_list = []
+    energy_delay_prod_list = []
+    energy_eff_list = []
+    power_list = []
+    power_eff_list = []
 
-    print("# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ")
-    print("Processing " + arch_list[0] + ":")
-    print("# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ")
+    for t in range(total_timestamp):
+        run_name = run_prefix+str(t)+run_postfix
+        run_dir = dir_root + run_name + "/"
+        print("Processing dir: " + dir_root + run_name)
+
+        area_file = run_dir + "simEffOut/" + run_name + "_area.csv"
+        throughput_file = run_dir + "simHwOut/" + run_name + "_throughput_real.csv"
+        energy_file = run_dir + "simEffOut/" + run_name + "_energy.csv"
+        power_file = run_dir + "simEffOut/" + run_name + "_power.csv"
     
-    # binary parallel
-    computing = "bp"
-    name = arch_list[0] + "_" + bit_list[0].zfill(2) + "b_" + computing + "_" + "001c_" + network_list[0] + "_" + ram_list[0]
-    if not os.path.exists("./outputs/" + technode + "/" + name):
-        raise ValueError("Folder ./outputs/" + technode + "/" + name + " does not exist.")
-    
-    path = "./outputs/" + technode + "/" + name + "/simHwOut/"
-    cycle_list.append(return_indexed_elems(  input_csv=path + name + "_throughput_real.csv", index=2)) # total cycle
-                    
+        if t == 0:
+            area_list.append(return_indexed_elems(input_csv=area_file, index=0)) # dram area
+            area_list.append(return_indexed_elems(input_csv=area_file, index=8)) # sram area
+            area_list.append(return_indexed_elems(input_csv=area_file, index=13)) # sa area
+            area_list.append(return_indexed_elems(input_csv=area_file, index=14)) # total area
+
+        runtime_list.append(return_indexed_elems(input_csv=throughput_file, index=3)) # total real runtime
+        
+        energy_list_temp = []
+        energy_list_temp.append(return_indexed_elems(input_csv=energy_file, index=8)) # dram energy
+        energy_list_temp.append(return_indexed_elems(input_csv=energy_file, index=18)) # sram energy
+        energy_list_temp.append(return_indexed_elems(input_csv=energy_file, index=33)) # sa energy
+        energy_list_temp.append(return_indexed_elems(input_csv=energy_file, index=36)) # total energy
+        energy_list.append(energy_list_temp)
+        energy_delay_prod_list.append([e * runtime_list[-1] for e in energy_list_temp])
+        energy_eff_list.append([e/runtime_list[-1] for e in energy_list_temp])
+
+        power_list_temp = []
+        power_list_temp.append(return_indexed_elems(input_csv=power_file, index=8)) # dram power
+        power_list_temp.append(return_indexed_elems(input_csv=power_file, index=18)) # sram power
+        power_list_temp.append(return_indexed_elems(input_csv=power_file, index=33)) # sa power
+        power_list_temp.append(return_indexed_elems(input_csv=power_file, index=36)) # total power
+        power_list.append(power_list_temp)
+        power_eff_list.append([p/runtime_list[-1] for p in power_list_temp])
+
+    return area_list, runtime_list, energy_list, energy_delay_prod_list, energy_eff_list, power_list, power_eff_list
+
 
 def prune(input_list):
     l = []
