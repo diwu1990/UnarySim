e9ddeb715b2476f1639345186d4dc34761c20146
diwu1990 <diwu0111@hotmail.com>
2021-12-05 11:42:09 -0600

    rewrite shiftreg api

--- kernel/shiftreg.py
+++ kernel/shiftreg.py
@@ -2,26 +2,38 @@ import torch
 
 class ShiftReg(torch.nn.Module):
     """
-    this module is a shift register for tensor.
-    it takes in unary bits and output shifter register value, as well as the total number of 1s in current shift register.
+    This module is a shift register.
+    Its input is bitstreams.
+    Its output are shifter register value and the one count in the shift register.
     """
-    def __init__(self,
-                 depth=8,
-                 stype=torch.float):
+    def __init__(
+        self,
+        hwcfg={
+            "entry" : 8
+        }, 
+        swcfg={
+            "stype" : torch.float
+        }):
         super(ShiftReg, self).__init__()
-        self.depth = depth
-        self.sr = torch.nn.Parameter(torch.tensor([x%2 for x in range(0, depth)]).type(stype), requires_grad=False)
-        self.init = torch.nn.Parameter(torch.ones(1).type(torch.bool), requires_grad=False)
-        self.stype = stype
+        self.hwcfg = {}
+        self.hwcfg["entry"] = hwcfg["entry"]
+
+        self.swcfg = {}
+        self.swcfg["stype"] = swcfg["stype"]
+
+        self.entry = hwcfg["entry"]
+        self.stype = swcfg["stype"]
+        self.sr = torch.nn.Parameter(torch.tensor([x%2 for x in range(0, self.entry)]).type(self.stype), requires_grad=False)
+        self.first = True
 
     def ShiftReg_forward(self, input, mask=None, index=0):
-        if self.init.item() is True:
+        if self.first is True:
             new_shape = [1 for _ in range(len(input.shape))]
-            new_shape.insert(0, self.depth)
+            new_shape.insert(0, self.entry)
             self.sr.data = input.repeat(new_shape)
-            for i in range(self.depth):
+            for i in range(self.entry):
                 self.sr[i].fill_(i%2)
-            self.init.data.fill_(False)
+            self.first = False
         
         # do shifting
         # output
@@ -30,12 +42,12 @@ class ShiftReg(torch.nn.Module):
         cnt = torch.sum(self.sr, 0)
         if mask is None:
             self.sr.data = torch.roll(self.sr, -1, 0)
-            self.sr.data[self.depth-1] = input.clone().detach()
+            self.sr.data[self.entry-1] = input.clone().detach()
         else:
-            assert mask.size() == input.size(), "Size of the enable mask unmatches that of input"
+            assert mask.size() == input.size(), "Error: size of the enable mask unmatches that of input in " + self + " class."
             mask_val = mask.type(self.stype)
             sr_shift = torch.roll(self.sr, -1, 0)
-            sr_shift[self.depth-1] = input.clone().detach()
+            sr_shift[self.entry-1] = input.clone().detach()
             sr_no_shift = self.sr.clone().detach()
             self.sr.data = mask_val * sr_shift + (1 - mask_val) * sr_no_shift
         return out, cnt
--- test/kernel/test_kernel_shiftreg.py
+++ test/kernel/test_kernel_shiftreg.py
@@ -1,27 +1,31 @@
-# %%
 import torch
-from UnarySim.kernel.shiftreg import ShiftReg
-from UnarySim.stream.gen import RNG, SourceGen, BSGen
-from UnarySim.metric.metric import ProgError
-import matplotlib.pyplot as plt
-import time
-import math
-import numpy as np
+from UnarySim.kernel import ShiftReg
 
-# %%
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# %%
-depth = 4
-stype = torch.int8
-sr = ShiftReg(depth=4, stype=stype).to(device)
+def test_shiftreg():
+    hwcfg = {
+            "width" : 12,
+            "mode" : "bipolar",
+            "dim" : 0,
+            "rng" : "sobol",
+            "scale" : 1,
+            "depth" : 4,
+            "entry" : 4
+        }
+    swcfg = {
+            "rtype" : torch.float,
+            "stype" : torch.float,
+            "btype" : torch.float
+        }
+    sr = ShiftReg(hwcfg, swcfg).to(device)
 
-# %%
-a = torch.tensor([[1, 1], [1, 0], [0, 1], [0, 0]]).type(stype).to(device)
+    a = torch.tensor([[1, 1], [1, 0], [0, 1], [0, 0]]).type(swcfg["stype"]).to(device)
 
-# %%
-oBit, cnt = sr(a, mask=torch.tensor([[1, 1], [1, 1], [0, 0], [0, 0]]).to(device))
-print(oBit, cnt)
-print(sr.sr)
+    oBit, cnt = sr(a, mask=torch.tensor([[1, 1], [1, 1], [0, 0], [0, 0]]).to(device))
+    print(oBit, cnt)
+    print(sr.sr)
 
-# %%
+
+if __name__ == '__main__':
+    test_shiftreg()
