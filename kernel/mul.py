a18beb088a90512f8805cee9ebff7b61b1256834
diwu1990 <diwu0111@hotmail.com>
2021-12-05 14:43:17 -0600

    rewrite mul api

--- kernel/mul.py
+++ kernel/mul.py
@@ -1,112 +1,108 @@
 import torch
-from UnarySim.stream.gen import RNG, SourceGen, BSGen
-from UnarySim.kernel.shiftreg import ShiftReg
+from UnarySim.stream import RNG, BinGen, BSGen
+from UnarySim.kernel import ShiftReg
 
 class FSUMul(torch.nn.Module):
     """
-    this module is for unary multiplication, supporting static/non-static computation, unipolar/bipolar
-    input_prob_1 is a don't care if the multiplier is non-static, defualt value is None.
-    if the multiplier is static, then need to input the pre-scaled input_1 to port input_prob_1 
+    This module is for unary multiplication, supporting static/in-stream, unipolar/bipolar.
+    If the multiplier is static, then need to input the pre-scaled in_1 to port in_1_prob.
+    If the multiplier is in-stream, in_1_prob is ignored.
+    Please refer to
+    1) uGEMM: Unary Computing Architecture for GEMM Applications
+    2) uGEMM: Unary Computing for GEMM Applications
     """
-    def __init__(self,
-                 bitwidth=8,
-                 mode="bipolar",
-                 static=False,
-                 input_prob_1=None,
-                 rtype=torch.float,
-                 stype=torch.float):
+    def __init__(
+        self,
+        in_1_prob=None,
+        hwcfg={
+            "width" : 8,
+            "mode" : "bipolar",
+            "static" : False,
+            "rng" : "Sobol",
+            "dimr" : 1
+        },
+        swcfg={
+            "rtype" : torch.float,
+            "stype" : torch.float
+        }):
         super(FSUMul, self).__init__()
+
+        self.hwcfg={}
+        self.hwcfg["width"] = hwcfg["width"]
+        self.hwcfg["mode"] = hwcfg["mode"].lower()
+        self.hwcfg["static"] = hwcfg["static"]
+        self.hwcfg["rng"] = hwcfg["rng"]
+        self.hwcfg["dimr"] = hwcfg["dimr"]
+
+        self.swcfg={}
+        self.swcfg["rtype"] = swcfg["rtype"]
+        self.swcfg["stype"] = swcfg["stype"]
+
+        self.entry = 2**hwcfg["width"]
+        self.static = hwcfg["static"]
+        self.stype = swcfg["stype"]
+        self.rtype = swcfg["rtype"]
         
-        self.bitwidth = bitwidth
-        self.depth = 2**bitwidth
-        self.mode = mode
-        self.static = static
-        self.stype = stype
-        self.rtype = rtype
-        
-        assert self.mode == "unipolar" or self.mode == "bipolar", "Unsupported mode in FSUMul."
+        self.mode = hwcfg["mode"].lower()
+        assert self.mode == "unipolar" or "bipolar", \
+            "Error: the hw config 'mode' in " + self + " class requires one of ['unipolar', 'bipolar']."
 
         # the random number generator used in computation
-        self.rng = RNG(
-            bitwidth=self.bitwidth,
-            dim=1,
-            rng="Sobol",
-            rtype=self.rtype)()
+        self.rng = RNG(hwcfg, swcfg)()
         
         if self.static is True:
-            # the probability of input_1 used in static computation
-            self.input_prob_1 = input_prob_1
-            assert input_prob_1 is not None, "Static multiplier requires input_prob_1."
+            # the probability of in_1 used in static computation
+            self.in_1_prob = in_1_prob
+            assert in_1_prob is not None, "Error: the static multiplier requires in_1_prob."
             # directly create an unchange bitstream generator for static computation
-            self.source_gen = SourceGen(self.input_prob_1, self.bitwidth, self.mode, self.rtype)()
-            self.bsg = BSGen(self.source_gen, self.rng, torch.int8)
+            self.source_gen = BinGen(self.in_1_prob, hwcfg, swcfg)()
+            self.bsg = BSGen(self.source_gen, self.rng, {"stype" : torch.int8})
             # rng_idx is used later as an enable signal, get update every cycled
             self.rng_idx = torch.nn.Parameter(torch.zeros(1).type(torch.long), requires_grad=False)
             
             # Generate two seperate bitstream generators and two enable signals for bipolar mode
             if self.mode == "bipolar":
-                self.bsg_inv = BSGen(self.source_gen, self.rng, torch.int8)
+                self.bsg_inv = BSGen(self.source_gen, self.rng, {"stype" : torch.int8})
                 self.rng_idx_inv = torch.nn.Parameter(torch.zeros(1).type(torch.long), requires_grad=False)
         else:
             # use a shift register to store the count of 1s in one bitstream to generate data
-            self.sr = ShiftReg(depth=self.depth, stype=self.stype)
+            sr_hwcfg={
+                "entry" : self.entry
+            }
+            self.sr = ShiftReg(sr_hwcfg, swcfg)
             self.rng_idx = torch.nn.Parameter(torch.zeros(1).type(torch.long), requires_grad=False)
             if self.mode == "bipolar":
                 self.rng_idx_inv = torch.nn.Parameter(torch.zeros(1).type(torch.long), requires_grad=False)
 
-    def FSUMul_forward(self, input_0, input_1=None):
+    def FSUMul_forward(self, in_0, in_1=None):
         if self.static is True:
             # for input0 is 0.
-            path = input_0.type(torch.int8) & self.bsg(self.rng_idx)
+            path = in_0.type(torch.int8) & self.bsg(self.rng_idx)
             # conditional update for rng index when input0 is 1. The update simulates enable signal of bs gen.
-            self.rng_idx.data = self.rng_idx.add(input_0.type(torch.long))
+            self.rng_idx.data = self.rng_idx.add(in_0.type(torch.long))
             
             if self.mode == "unipolar":
                 return path
             else:
                 # for input0 is 0.
-                path_inv = (1 - input_0.type(torch.int8)) & (1 - self.bsg_inv(self.rng_idx_inv))
+                path_inv = (1 - in_0.type(torch.int8)) & (1 - self.bsg_inv(self.rng_idx_inv))
                 # conditional update for rng_idx_inv
-                self.rng_idx_inv.data = self.rng_idx_inv.add(1 - input_0.type(torch.long))
+                self.rng_idx_inv.data = self.rng_idx_inv.add(1 - in_0.type(torch.long))
                 return path | path_inv
         else:
-            _, source = self.sr(input_1)
-            path = input_0.type(torch.int8) & torch.gt(source, self.rng[self.rng_idx]).type(torch.int8)
-            self.rng_idx.data = self.rng_idx.add(input_0.type(torch.long)) % self.depth
+            _, source = self.sr(in_1)
+            path = in_0.type(torch.int8) & torch.gt(source, self.rng[self.rng_idx]).type(torch.int8)
+            self.rng_idx.data = self.rng_idx.add(in_0.type(torch.long)) % self.entry
 
             if self.mode == "unipolar":
                 return path
             else:
                 # for input0 is 0.
-                path_inv = (1 - input_0.type(torch.int8)) & (1 - torch.gt(source, self.rng[self.rng_idx_inv]).type(torch.int8))
+                path_inv = (1 - in_0.type(torch.int8)) & (1 - torch.gt(source, self.rng[self.rng_idx_inv]).type(torch.int8))
                 # conditional update for rng_idx_inv
-                self.rng_idx_inv.data = self.rng_idx_inv.add(1 - input_0.type(torch.long)) % self.depth
+                self.rng_idx_inv.data = self.rng_idx_inv.add(1 - in_0.type(torch.long)) % self.entry
                 return path | path_inv
             
-    def forward(self, input_0, input_1=None):
-        return self.FSUMul_forward(input_0, input_1).type(self.stype)
+    def forward(self, in_0, in_1=None):
+        return self.FSUMul_forward(in_0, in_1).type(self.stype)
 
-    
-class GainesMul(torch.nn.Module):
-    """
-    this module is for Gaines stochastic multiplication, supporting unipolar/bipolar
-    """
-    def __init__(self,
-                 mode="bipolar",
-                 stype=torch.float):
-        super(GainesMul, self).__init__()
-        self.mode = mode
-        self.stype = stype
-
-    def GainesMul_forward(self, input_0, input_1):
-        if self.mode == "unipolar":
-            return input_0.type(torch.int8) & input_1.type(torch.int8)
-        elif self.mode == "bipolar":
-            return 1 - (input_0.type(torch.int8) ^ input_1.type(torch.int8))
-        else:
-            raise ValueError("GainesMul mode is not implemented.")
-            
-    def forward(self, input_0, input_1):
-        return self.GainesMul_forward(input_0, input_1).type(self.stype)
-    
-    
---
+++ test/kernel/test_kernel_fsumul.py
@@ -0,0 +1,73 @@
+import torch
+from UnarySim.kernel.mul import FSUMul
+from UnarySim.stream.gen import RNG, BinGen, BSGen
+from UnarySim.metric.metric import ProgError
+import matplotlib.pyplot as plt
+import time
+import math
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def test_fsumul():
+    hwcfg = {
+            "width" : 8,
+            "mode" : "bipolar",
+            "dimr" : 1,
+            "dima" : 0,
+            "rng" : "sobol",
+            "scale" : 1,
+            "depth" : 10,
+            "entry" : None,
+            "static" : True
+        }
+    swcfg = {
+            "rtype" : torch.float,
+            "stype" : torch.float,
+            "btype" : torch.float
+        }
+    bitwidth = hwcfg["width"]
+
+    col = 1
+    modes = ["bipolar", "unipolar"]
+
+    for mode in modes:
+        if mode == "unipolar":
+            input_prob = torch.rand(col).mul(2**bitwidth).round().div(2**bitwidth).to(device)
+            iVec = torch.rand(col).mul(2**bitwidth).round().div(2**bitwidth).to(device)
+        elif mode == "bipolar":
+            input_prob = torch.rand(col).mul(2).sub(1).mul(2**bitwidth).round().div(2**bitwidth).to(device)
+            iVec = torch.rand(col).mul(2).sub(1).mul(2**bitwidth).round().div(2**bitwidth).to(device)
+
+        hwcfg["mode"] = mode
+
+        dut_mul = FSUMul(input_prob, hwcfg, swcfg).to(device)
+
+        oVec = torch.mul(iVec, input_prob).mul(2**bitwidth).round().div(2**bitwidth).to(device)
+
+        iVecPE = ProgError(iVec, hwcfg).to(device)
+        oVecPE = ProgError(oVec, hwcfg).to(device)
+
+        iVecSource = BinGen(iVec, hwcfg, swcfg)().to(device)
+        iVecRNG = RNG(hwcfg, swcfg)().to(device)
+        iVecBS = BSGen(iVecSource, iVecRNG, swcfg).to(device)
+
+        with torch.no_grad():
+            start_time = time.time()
+            for i in range(2**bitwidth):             # unary cycle count 2^n for n-bit binary data
+                iBS = iVecBS(torch.tensor([i]))      # input bit stream generation
+                iVecPE.Monitor(iBS)                  # input accuracy measurement
+                oVecU = dut_mul(iBS)                 # computing kernel, e.g., multiplication
+                oVecPE.Monitor(oVecU)                # output accuracy measurement
+            print("--- %s seconds ---" % (time.time() - start_time))
+            print("input error: ", torch.min(iVecPE()[1]), torch.max(iVecPE()[1]))
+            print("output error: ", torch.min(oVecPE()[1]), torch.max(oVecPE()[1]))
+            result_pe = oVecPE()[1].cpu().numpy()
+            print("RMSE", math.sqrt(sum(result_pe**2)/len(result_pe)))
+            print("bias", sum(result_pe)/len(result_pe))
+            # fig = plt.hist(result_pe, bins='auto')  # arguments are passed to np.histogram
+            # plt.title("Histogram for final output error")
+            # plt.show()
+
+
+if __name__ == '__main__':
+    test_fsumul()
