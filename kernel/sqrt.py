93ed70c68296b732c75924bae0965810d2efffdd
diwu1990 <diwu0111@hotmail.com>
2021-12-05 17:54:55 -0600

    rewrite sqrt api

--- kernel/sqrt.py
+++ kernel/sqrt.py
@@ -1,50 +1,129 @@
 import torch
-from UnarySim.stream.gen import RNG
-from UnarySim.stream.shuffle import Bi2Uni
-from UnarySim.kernel.shiftreg import ShiftReg
-from UnarySim.kernel.jkff import JKFF
-from UnarySim.kernel.div import CORDIV_kernel
-from UnarySim.kernel.add import FSUAdd
+from UnarySim.stream import RNG
+from UnarySim.stream import Bi2Uni
+from UnarySim.kernel import ShiftReg
+from UnarySim.kernel import JKFF
+from UnarySim.kernel import CORDIV_kernel
+from UnarySim.kernel import FSUAdd
 import math
 
 class FSUSqrt(torch.nn.Module):
     """
-    this module is for unary square root, including iscbdiv-based and jkdiv-based.
+    This module is for unary square root, including iscbdiv-based and jkdiv-based. Please refer to
+    1) "In-Stream Stochastic Division and Square Root via Correlation"
+    2) "In-Stream Correlation-Based Division and Bit-Inserting Square Root in Stochastic Computing"
     """
-    def __init__(self, 
-                 mode="bipolar", 
-                 jk_trace=True, 
-                 depth_kernel=1, 
-                 rng="Sobol", 
-                 rng_dim=4, 
-                 emit=True, 
-                 depth_sr=2, 
-                 stype=torch.float):
+    def __init__(
+        self, 
+        hwcfg={
+            "mode" : "bipolar",
+            "jk_trace" : True,
+            "emit" : True,
+            "entry_kn" : 1,
+            "entry_sr" : 2,
+            "rng" : "Sobol",
+            "dimr" : 4
+        },
+        swcfg={
+            "stype" : torch.float,
+            "btype" : torch.float
+        }):
         super(FSUSqrt, self).__init__()
+        self.hwcfg = {}
+        self.hwcfg["mode"] = hwcfg["mode"].lower()
+        self.hwcfg["jk_trace"] = hwcfg["jk_trace"]
+        self.hwcfg["emit"] = hwcfg["emit"]
+        self.hwcfg["entry_kn"] = hwcfg["entry_kn"]
+        self.hwcfg["entry_sr"] = hwcfg["entry_sr"]
+        self.hwcfg["rng"] = hwcfg["rng"]
+        self.hwcfg["dimr"] = hwcfg["dimr"]
+
+        self.swcfg = {}
+        self.swcfg["btype"] = swcfg["btype"]
+        self.swcfg["stype"] = swcfg["stype"]
+
+        # data representation
+        self.mode = hwcfg["mode"].lower()
+        assert self.mode == "unipolar" or "bipolar", \
+            "Error: the hw config 'mode' in " + self + " class requires one of ['unipolar', 'bipolar']."
         
-        assert math.ceil(math.log2(depth_kernel)) == math.floor(math.log2(depth_kernel)) , "Input depth_kernel needs to be power of 2."
-        assert math.ceil(math.log2(depth_sr)) == math.floor(math.log2(depth_sr)) , "Input depth_sr needs to be power of 2."
-        self.mode = mode
-        self.stype = stype
-        self.jk_trace = jk_trace
-        self.emit = emit
-        if emit is True:
+        self.jk_trace = hwcfg["jk_trace"]
+        self.emit = hwcfg["emit"]
+        self.entry_kn = hwcfg["entry_kn"]
+        self.entry_sr = hwcfg["entry_sr"]
+        assert math.ceil(math.log2(self.entry_kn)) == math.floor(math.log2(self.entry_kn)) , \
+            "Eroor: the hw config 'entry_kn' in " + self + " class needs to be power of 2."
+        assert math.ceil(math.log2(self.entry_sr)) == math.floor(math.log2(self.entry_sr)) , \
+            "Eroor: the hw config 'entry_sr' in " + self + " class needs to be power of 2."
+
+        self.stype = swcfg["stype"]
+        if self.emit is True:
             self.emit_out = torch.nn.Parameter(torch.zeros(1).type(torch.int8), requires_grad=False)
-            self.nsadd = FSUAdd(mode="unipolar", scaled=False, acc_dim=0, stype=torch.int8)
-            self.sr = ShiftReg(depth_sr, stype=torch.int8)
-            self.depth_sr = depth_sr
-            self.rng = RNG(int(math.log2(depth_sr)), rng_dim, rng, torch.long)()
+            hwcfg_add={
+                "mode" : "unipolar",
+                "scale" : 1,
+                "dima" : 0,
+                "depth" : 10,
+                "entry" : None,
+            }
+            swcfg_add={
+                "btype" : swcfg["btype"],
+                "stype" : torch.int8,
+            }
+            self.nsadd = FSUAdd(hwcfg_add, swcfg_add)
+            hwcfg_sr={
+                "entry" : self.entry_sr
+            }
+            swcfg_sr={
+                "btype" : swcfg["btype"],
+                "stype" : torch.int8,
+            }
+            self.sr = ShiftReg(hwcfg_sr, swcfg_sr)
+            hwcfg_rng={
+                "width" : int(math.log2(self.entry_sr)), 
+                "dimr" : 1, 
+                "rng" : hwcfg["rng"]
+            }
+            swcfg_rng={
+                "rtype" : torch.long
+            }
+            self.rng = RNG(hwcfg_rng, swcfg_rng)()
             self.idx = torch.nn.Parameter(torch.zeros(1).type(torch.long), requires_grad=False)
-            if mode == "bipolar":
-                self.bi2uni_emit = Bi2Uni(stype=torch.int8)
+            if self.mode == "bipolar":
+                hwcfg_b2u={
+                    "depth" : 3
+                }
+                swcfg_b2u={
+                    "btype" : swcfg["btype"],
+                    "stype" : torch.int8
+                }
+                self.bi2uni_emit = Bi2Uni(hwcfg_b2u, swcfg_b2u)
         else:
             self.trace = torch.nn.Parameter(torch.zeros(1).type(torch.int8), requires_grad=False)
-            if mode == "bipolar":
-                self.bi2uni = Bi2Uni(stype=torch.int8)
-            if jk_trace is True:
-                self.jkff = JKFF(stype=torch.int8)
+            if self.mode == "bipolar":
+                hwcfg_b2u={
+                    "depth" : 3
+                }
+                swcfg_b2u={
+                    "btype" : swcfg["btype"],
+                    "stype" : torch.int8
+                }
+                self.bi2uni = Bi2Uni(hwcfg_b2u, swcfg_b2u)
+            if self.jk_trace is True:
+                swcfg_jkff={
+                    "stype" : torch.int8
+                }
+                self.jkff = JKFF(swcfg_jkff)
             else:
-                self.cordiv_kernel = CORDIV_kernel(depth=depth_kernel, rng=rng, rng_dim=rng_dim, stype=torch.int8)
+                hwcfg_cordiv_kernel={
+                    "entry" : 4,
+                    "rng" : hwcfg["rng"],
+                    "dimr" : 4
+                }
+                swcfg_cordiv_kernel={
+                    "stype" : torch.int8
+                }
+                self.cordiv_kernel = CORDIV_kernel(hwcfg_cordiv_kernel, swcfg_cordiv_kernel)
                 self.dff = torch.nn.Parameter(torch.zeros(1).type(torch.int8), requires_grad=False)
         
     def bipolar_trace(self, output):
@@ -75,13 +154,13 @@ class FSUSqrt(torch.nn.Module):
     
     def unipolar_emit(self, output):
         output_inv = 1 - output
-        output_inv_scrambled, dontcare = self.sr(output_inv, index=self.idx.item()%self.depth_sr)
+        output_inv_scrambled, dontcare = self.sr(output_inv, index=self.idx.item()%self.entry_sr)
         emit_out = output_inv_scrambled & output
         return emit_out
     
     def bipolar_emit(self, output):
         output_inv = 1 - output
-        output_inv_scrambled, dontcare = self.sr(output_inv, index=self.idx.item()%self.depth_sr)
+        output_inv_scrambled, dontcare = self.sr(output_inv, index=self.idx.item()%self.entry_sr)
         output_uni = self.bi2uni_emit(output)
         emit_out = output_inv_scrambled & output_uni
         return emit_out
@@ -102,55 +181,5 @@ class FSUSqrt(torch.nn.Module):
                 self.trace.data = self.bipolar_trace(output)
             else:
                 self.trace.data = self.unipolar_trace(output)
-        return output.type(self.stype)
-    
-
-class GainesSqrt(torch.nn.Module):
-    """
-    this module is for Gaines square root.
-    """
-    def __init__(self, 
-                 depth=5, 
-                 mode="bipolar", 
-                 rng="Sobol", 
-                 rng_dim=1, 
-                 stype=torch.float):
-        super(GainesSqrt, self).__init__()
-        
-        # data representation
-        self.mode = mode
-        self.scnt_max = torch.nn.Parameter(torch.tensor([2**depth-1]).type(torch.float), requires_grad=False)
-        self.scnt = torch.nn.Parameter(torch.tensor([2**(depth-1)]).type(torch.float), requires_grad=False)
-        self.rng = RNG(depth, rng_dim, rng, torch.float)()
-        self.rng_idx = torch.nn.Parameter(torch.zeros(1).type(torch.long), requires_grad=False)
-        self.out_d = torch.nn.Parameter(torch.zeros(1).type(torch.int8), requires_grad=False)
-        self.stype = stype
-        
-    def forward(self, input):
-        # output is the same for both bipolar and unipolar
-        output = torch.gt(self.scnt, self.rng[self.rng_idx%self.rng.numel()]).type(torch.int8)
-        self.rng_idx.data = self.rng_idx + 1
-        output = output + torch.zeros_like(input, dtype=torch.int8)
-        
-        if self.mode == "unipolar":
-            inc = input.type(torch.float)
-            dec = (output & self.out_d).type(torch.float)
-            self.out_d.data = output.type(torch.int8)
-        else:
-            # this is not a good implementation
-            # prod = 1 - output ^ self.out_d
-            # inc = (input.type(torch.int8) & prod).type(torch.float)
-            # dec = ((1 - input).type(torch.int8) & (1 - prod)).type(torch.float)
-            # self.out_d.data = output.type(torch.int8)
-            
-            inc = input.type(torch.float)
-            dec = (1 - output ^ self.out_d).type(torch.float)
-            self.out_d.data = output.type(torch.int8)
-            
-        # scnt is also the same in terms of the up/down behavior and comparison
-        self.scnt.data = (inc * (self.scnt + 1) + (1 - inc) * self.scnt).view(input.size())
-        self.scnt.data = (dec * (self.scnt - 1) + (1 - dec) * self.scnt)
-        self.scnt.data = self.scnt.clamp(0, self.scnt_max.item())
-        
         return output.type(self.stype)
     
