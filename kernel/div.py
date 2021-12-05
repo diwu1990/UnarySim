a11353c13f9331b5a0edee1722ce4e3ed2065e06
diwu1990 <diwu0111@hotmail.com>
2021-12-05 13:21:22 -0600

    rewrite div api

--- kernel/div.py
+++ kernel/div.py
@@ -1,87 +1,134 @@
 import torch
-from UnarySim.stream.gen import RNG
-from UnarySim.stream.shuffle import SkewedSync, Bi2Uni, Uni2Bi
-from UnarySim.kernel.shiftreg import ShiftReg
-from UnarySim.kernel.abs import FSUAbs
+from UnarySim.stream import RNG
+from UnarySim.stream import SkewedSync, Bi2Uni, Uni2Bi
+from UnarySim.kernel import ShiftReg
+from UnarySim.kernel import FSUSignAbs
 import math
 
 class CORDIV_kernel(torch.nn.Module):
     """
-    the kernel of the correlated divivison
-    this kernel is for unipolar only
-    dividend and divisor have to synchronized
+    The kernel of the correlated divivison, for unipolar only
+    The dividend and divisor have to be synchronized before fed to this kernle
     """
-    def __init__(self, 
-                 depth=4, 
-                 rng="Sobol", 
-                 rng_dim=4, 
-                 stype=torch.float):
+    def __init__(
+        self, 
+        hwcfg={
+            "entry" : 4,
+            "rng" : "Sobol",
+            "dimr" : 4
+        },
+        swcfg={
+            "stype" : torch.float
+        }):
         super(CORDIV_kernel, self).__init__()
-        self.depth = depth
-        self.sr = ShiftReg(depth, stype)
-        self.rng = RNG(int(math.log2(depth)), rng_dim, rng, torch.long)()
-        self.idx = torch.nn.Parameter(torch.zeros(1).type(torch.float), requires_grad=False)
-        self.stype = stype
-        self.init = torch.nn.Parameter(torch.ones(1).type(torch.bool), requires_grad=False)
-        self.historic_q = torch.nn.Parameter(torch.ones(1).type(stype), requires_grad=False)
+        self.hwcfg = {}
+        self.hwcfg["entry"] = hwcfg["entry"]
+        self.hwcfg["rng"] = hwcfg["rng"]
+        self.hwcfg["dimr"] = hwcfg["dimr"]
+
+        self.swcfg = {}
+        self.swcfg["stype"] = swcfg["stype"]
+
+        self.entry = hwcfg["entry"]
+        self.sr = ShiftReg(self.hwcfg, self.swcfg)
+        hwcfg_rng = {
+            "width" : int(math.log2(self.entry)),
+            "dimr" : hwcfg["dimr"],
+            "rng" : hwcfg["rng"]
+        }
+        swcfg_rng = {
+            "rtype" : torch.long
+        }
+        self.rng = RNG(hwcfg_rng, swcfg_rng)()
+        self.idx = torch.nn.Parameter(torch.zeros(1).type(torch.long), requires_grad=False)
+        self.stype = swcfg["stype"]
+        self.historic_q = torch.nn.Parameter(torch.ones(1).type(self.stype), requires_grad=False)
         
     def forward(self, dividend, divisor):
         # generate the random number to index the shift register
-        # 1) generate based on divisor value, conditional probability
-        # if self.init.item() is True:
-        #     self.historic_q = torch.gather(self.sr.sr, 0, self.rng[self.idx.type(torch.long)%self.depth].type(torch.long))
-        #     self.init.data.fill_(False)
-        # else:
-        #     self.historic_q = torch.gather(self.sr.sr, 0, torch.unsqueeze(self.rng[self.idx.type(torch.long)%self.depth].type(torch.long), 0))
-        # divisor_eq_1 = torch.eq(divisor, 1).type(self.stype)
-        # self.idx.data = self.idx.add(divisor_eq_1)
-        
-        # 2) always generating, no need to deal conditional probability
+        # always generating, no need to deal conditional probability
         divisor_eq_1 = torch.eq(divisor, 1).type(self.stype)
-        self.historic_q.data = self.sr.sr[self.rng[self.idx.type(torch.long)%self.depth]]
+        self.historic_q.data = self.sr.sr[self.rng[self.idx%self.entry]]
         self.idx.data = self.idx.add(1)
         
         quotient = (divisor_eq_1 * dividend + (1 - divisor_eq_1) * self.historic_q).view(dividend.size())
         
-        # shift register update 
-        # 1) update shift register based on whether divisor is valid
+        # shift register update based on whether divisor is valid
         dontcare1, dontcare2 = self.sr(quotient.type(self.stype), mask=divisor_eq_1)
         
-        # 2) always update shift register
-        # dontcare1, dontcare2 = self.sr(quotient.type(self.stype), mask=None)
-
         return quotient.type(self.stype)
     
     
 class FSUDiv(torch.nn.Module):
     """
-    this module is for fully streaming unary div, i.e., iscbdiv.
+    This module is for fully streaming unary div, i.e., iscbdiv. Please refer to
+    1) "In-Stream Stochastic Division and Square Root via Correlation"
+    2) "In-Stream Correlation-Based Division and Bit-Inserting Square Root in Stochastic Computing"
     """
-    def __init__(self, 
-                 depth_abs=3, 
-                 depth_sync=2, 
-                 depth_kernel=2, 
-                 shiftreg_abs=False, 
-                 mode="bipolar", 
-                 rng="Sobol", 
-                 rng_dim=4, 
-                 btype=torch.float, 
-                 stype=torch.float):
+    def __init__(
+        self, 
+        hwcfg={
+            "depth_sa" : 3,
+            "depth_ss" : 2,
+            "entry_kn" : 2,
+            "mode" : "bipolar",
+            "rng" : "Sobol",
+            "dimr" : 4
+        },
+        swcfg = {
+            "stype" : torch.float,
+            "btype" : torch.float
+        }):
         super(FSUDiv, self).__init__()
-        
+        self.hwcfg = {}
+        self.hwcfg["depth_sa"] = hwcfg["depth_sa"]
+        self.hwcfg["depth_ss"] = hwcfg["depth_ss"]
+        self.hwcfg["entry_kn"] = hwcfg["entry_kn"]
+        self.hwcfg["mode"] = hwcfg["mode"].lower()
+        self.hwcfg["rng"] = hwcfg["rng"]
+        self.hwcfg["dimr"] = hwcfg["dimr"]
+
+        self.swcfg = {}
+        self.swcfg["btype"] = swcfg["btype"]
+        self.swcfg["stype"] = swcfg["stype"]
+
+
         # data representation
-        self.mode = mode
-        self.stype = stype
-        
+        self.mode = hwcfg["mode"].lower()
+        assert self.mode == "unipolar" or "bipolar", \
+            "Error: the hw config 'mode' in " + self + " class requires one of ['unipolar', 'bipolar']."
+        self.stype = swcfg["stype"]
+        self.btype = swcfg["btype"]
+        self.depth_sa = hwcfg["depth_sa"]
+        self.depth_ss = hwcfg["depth_ss"]
+        self.entry_kn = hwcfg["entry_kn"]
+
         if self.mode == "bipolar":
-            self.abs_dividend = FSUAbs(depth=depth_abs, shiftreg=shiftreg_abs, stype=stype, btype=btype)
-            self.abs_divisor  = FSUAbs(depth=depth_abs, shiftreg=shiftreg_abs, stype=stype, btype=btype)
-            self.bi2uni_dividend = Bi2Uni(stype=stype)
-            self.bi2uni_divisor  = Bi2Uni(stype=stype)
-            self.uni2bi_quotient = Uni2Bi(stype=stype)
-            
-        self.ssync = SkewedSync(depth=depth_sync, stype=stype, btype=btype)
-        self.cordiv_kernel = CORDIV_kernel(depth=depth_kernel, rng=rng, rng_dim=rng_dim, stype=stype)
+            hwcfg_sa = {
+                "depth" : self.depth_sa
+            }
+            self.abs_dividend = FSUSignAbs(hwcfg_sa, swcfg)
+            self.abs_divisor  = FSUSignAbs(hwcfg_sa, swcfg)
+            hwcfg_b2u = {
+                "depth" : 3
+            }
+            self.bi2uni_dividend = Bi2Uni(hwcfg_b2u, swcfg)
+            self.bi2uni_divisor  = Bi2Uni(hwcfg_b2u, swcfg)
+            hwcfg_u2b = {
+                "depth" : 4
+            }
+            self.uni2bi_quotient = Uni2Bi(hwcfg_u2b, swcfg)
+        
+        hwcfg_ss = {
+            "depth" : self.depth_ss
+        }
+        self.ssync = SkewedSync(hwcfg_ss, swcfg)
+        hwcfg_kn = {
+            "entry" : self.entry_kn,
+            "rng" : hwcfg["rng"],
+            "dimr" : hwcfg["dimr"]
+        }
+        self.cordiv_kernel = CORDIV_kernel(hwcfg_kn, swcfg)
         
     def bipolar_forward(self, dividend, divisor):
         sign_dividend, abs_dividend = self.abs_dividend(dividend)
@@ -103,55 +150,5 @@ class FSUDiv(torch.nn.Module):
             output = self.bipolar_forward(dividend, divisor)
         else:
             output = self.unipolar_forward(dividend, divisor)
-        return output.type(self.stype)
-    
-
-class GainesDiv(torch.nn.Module):
-    """
-    this module is for Gaines division.
-    """
-    def __init__(self, 
-                 depth=5, 
-                 mode="bipolar", 
-                 rng="Sobol", 
-                 rng_dim=1, 
-                 stype=torch.float):
-        super(GainesDiv, self).__init__()
-        
-        # data representation
-        self.mode = mode
-        self.scnt_max = torch.nn.Parameter(torch.tensor([2**depth-1]).type(torch.float), requires_grad=False)
-        self.scnt = torch.nn.Parameter(torch.tensor([2**(depth-1)]).type(torch.float), requires_grad=False)
-        self.rng = RNG(depth, rng_dim, rng, torch.float)()
-        self.rng_idx = torch.nn.Parameter(torch.zeros(1).type(torch.long), requires_grad=False)
-        self.divisor_d = torch.nn.Parameter(torch.zeros(1).type(torch.int8), requires_grad=False)
-        self.stype = stype
-        
-    def forward(self, dividend, divisor):
-        # output is the same for both bipolar and unipolar
-        output = torch.gt(self.scnt, self.rng[self.rng_idx%self.rng.numel()]).type(torch.int8)
-        self.rng_idx.data = self.rng_idx + 1
-        output = output + torch.zeros_like(dividend, dtype=torch.int8)
-        
-        if self.mode == "unipolar":
-            inc = dividend.type(torch.float)
-            dec = (output & divisor.type(torch.int8)).type(torch.float)
-        else:
-            dd_ds = 1 - (dividend.type(torch.int8) ^ divisor.type(torch.int8))
-            ds_ds = 1 - (self.divisor_d ^ divisor.type(torch.int8))
-            self.divisor_d.data = divisor.type(torch.int8)
-            ds_ds_out = 1 - (ds_ds ^ (1 - output))
-            inc = (dd_ds & ds_ds_out).type(torch.float)
-            dec = ((1 - dd_ds) & (1 - ds_ds_out)).type(torch.float)
-            
-            #　following implementation is not good for accuracy due to fluctuation of negative output.
-            # inc = dividend.type(torch.float)
-            # dec = (1 - output ^ divisor.type(torch.int8)).type(torch.float)
-        
-        # scnt is also the same in terms of the up/down behavior and comparison
-        self.scnt.data = (inc * (self.scnt + 1) + (1 - inc) * self.scnt).view(dividend.size())
-        self.scnt.data = (dec * (self.scnt - 1) + (1 - dec) * self.scnt)
-        self.scnt.data = self.scnt.clamp(0, self.scnt_max.item())
-        
         return output.type(self.stype)
     
